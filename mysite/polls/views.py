98111dc295fb3f323cd1cdc2ec3b92ae5e89c862
spacecreator8 <spcc888@ya.ru>
2023-11-03 15:15:03 +0700

    test

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+
+
+def redirectView(request):
+    return redirect('/polls')
 
 
 class IndexView(generic.ListView):
